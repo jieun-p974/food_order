ed8f8afa97898010dbd04c45ffca43a633b88a00
jieun-p974 <jieun.p974@gmail.com>
2022-07-28 21:01:37 +0900

    cart 추가 삭제, 장바구니 버튼 이펙트추가

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,14 +1,39 @@
+import { useContext, useEffect, useState } from "react";
 import classes from "./HeaderCartButton.module.css";
 import CartIcon from "../Cart/CartIcon";
+import CartContext from "../../store/cart-context";
 
 const HeaderCartButton = (props) => {
+  const [btnHighlighted, setBtnHighlighted] = useState(false);
+  const cartCtx = useContext(CartContext);
+  const { items } = cartCtx;
+  const numberOfCartItem = items.reduce((currNumber, item) => {
+    return currNumber + item.amount;
+  }, 0);
+  const btnClasses = `${classes.button} ${btnHighlighted ? classes.bump : ""}`;
+
+  useEffect(() => {
+    if (cartCtx.items.length === 0) {
+      return;
+    }
+    setBtnHighlighted(true);
+    // 300ms가 지나면 bump 클래스를 삭제함
+    const timer = setTimeout(() => {
+      setBtnHighlighted(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
+
   return (
-    <button className={classes.button}>
+    <button className={btnClasses} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
       <span>Your Cart</span>
-      <span className={classes.badge}>3</span>
+      <span className={classes.badge}>{numberOfCartItem}</span>
     </button>
   );
 };
